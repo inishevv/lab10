2073efb2f1cfc231d59e48aced2d76e5b8226b11
inishevv <146018080+inishevv@users.noreply.github.com>
2023-11-21 21:50:48 +0500

    проверка на число

--- lab10.py
+++ lab10.py
@@ -3,25 +3,49 @@ import logging
 
 logging.basicConfig(level=logging.INFO, filename="py_log.log",filemode="a")
 
+def chislo(ch):
+    while True:
+        user_ch = input(ch)
+        try:
+            numb = int(user_ch)
+            if numb >= 1:
+                return numb
+            else:
+                print('число должно быть больше 1')
+                logging.warning('число должно быть больше 1')
+        except ValueError:
+            print('Введено не число, попробуйте снова')
+            logging.warning('Введено не число, попробуйте снова')
+
 print('количество попыток пользователя: ')
-k = int(input())
+logging.warning('количество попыток пользователя: ')
+k = chislo('')
+logging.warning(k)
 print('максимальное число, которое может загадать компьютер: ')
-n = int(input())
+logging.warning('максимальное число, которое может загадать компьютер: ')
+n = chislo('')
+logging.warning(n)
 flag = 0
 
 number = random.randint(1,n)
+logging.warning('загаданное число:')
+logging.warning(number)
 
 for i in range(k):
-    print('введите число')
-    a = int(input())
-    logging.warning(a)
-    if a < number:
-        print("загаданное число больше")
-    elif a > number:
-        print("загаданное число меньше")
-    elif a == number:
-        print("вы выиграли!")
-        logging.warning('вы выиграли!')
-        flag += 1
+    if flag == 0:
+        print('введите число')
+        a = chislo('')
+        logging.warning(a)
+        if a < number:
+            print("загаданное число больше")
+            logging.warning("загаданное число больше")
+        elif a > number:
+            print("загаданное число меньше")
+            logging.warning("загаданное число меньше")
+        elif a == number:
+            print("вы выиграли!")
+            logging.warning('вы выиграли!')
+            flag += 1
 if flag == 0:
     print('попытки закончились')
+    logging.warning('попытки закончились')
